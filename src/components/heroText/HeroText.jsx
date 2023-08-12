33837f80344717eb90c419a19f54263d9a973561
Ashutosh <vashutosh625@gmail.com>
2023-08-12 22:28:44 +0530

    create a responsive navbar

--- src/components/heroText/HeroText.jsx
+++ src/components/heroText/HeroText.jsx
@@ -1,8 +1,8 @@
-
+import './HeroText.css'
 const HeroText = ()=>{
     return(
         <>
-            <div>
+            <div className="con">
                 <h1>DAILY WAGES</h1>
                 <h3>Directly connect to your local workers.</h3>
             </div>
