af8ebd4472f21aefb60b5deb05002c8c38c7d548
Ashutosh <vashutosh625@gmail.com>
2023-08-16 17:00:08 +0530

    added footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,18 @@
-
+import './Footer.css'
 const Footer = ()=>{
     return(
-        <>
-        </>
+        <footer>
+            <div className="footer-container">
+                <h4>Help Center</h4>
+            <h4>About Us</h4>
+            <h4>Contact Us</h4>
+            <h4>Privacy Policy</h4>
+            <h4>Disclaimer</h4>
+            <h4>Services</h4>
+            <h4>Terms Of Use</h4>
+            </div>
+            
+        </footer>
     )
 }
 
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -10,7 +10,7 @@ const HomePage = () => {
 			<div className="home">
 				<Hero />
 			</div>
-			<Footer />
+			
 		</>
 	);
 };
