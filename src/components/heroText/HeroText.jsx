409c5a346e0d54f33a92376f1a3cac253004b73d
Ashutosh <vashutosh625@gmail.com>
2023-08-18 19:15:13 +0530

    renamed all css file to module.css as there was conflict b/w class names of different components

--- src/components/heroText/HeroText.jsx
+++ src/components/heroText/HeroText.jsx
@@ -1,7 +1,7 @@
-import "./HeroText.css";
+import styles from "./HeroText.module.css";
 const HeroText = () => {
 	return (
-		<div className="hero-container">
+		<div className={styles['hero-container']}>
 			<h1>DAILY WAGES</h1>
 			<h3>Directly connect to your local workers.</h3>
 		</div>
--- src/components/popularService/PopularService.jsx
+++ src/components/popularService/PopularService.jsx
@@ -1,22 +1,22 @@
-import "./PopularService.css";
+import styles from "./PopularService.module.css";
 const PopularService = () => {
 	return (
-		<div className="popular-services-container">
+		<div className={styles["popular-services-container"]}>
 			<h2>Popular Services</h2>
-			<div className="services">
-				<div className="card">
+			<div className={styles.services}>
+				<div className={styles.card}>
 					<img src="./images/electrician.jpg" alt="electirician" />
 					<p>Electrician</p>
 				</div>
-				<div className="card">
+				<div className={styles.card}>
 					<img src="./images/painter.jpg" alt="painter" />
 					<p>Painter</p>
 				</div>
-				<div className="card">
+				<div className={styles.card}>
 					<img src="./images/plumber.jpg" alt="plumber" />
 					<p>Plumber</p>
 				</div>
-				<div className="card">
+				<div className={styles.card}>
 					<img src="./images/carpenter.jpg" alt="carpenter" />
 					<p>Carpenter</p>
 				</div>
