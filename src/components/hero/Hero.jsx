d001671b02fd16de9f91b0ed54d4b0cbc1956504
Ashutosh <vashutosh625@gmail.com>
2023-08-20 13:26:00 +0530

    made homepage responsive and improved some code

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,16 +1,14 @@
 import HeroText from "../heroText/HeroText";
 import SearchBar from "../searchBar/SearchBar";
 import PopularServices from "../popularService/PopularService";
-import Footer from '../footer/Footer';
-
+import styles from "./Hero.module.css";
 const Hero = () => {
 	return (
-		<>
+		<div className={styles.container}>
 			<HeroText />
 			<SearchBar />
 			<PopularServices />
-			<Footer />
-		</>
+		</div>
 	);
 };
 
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,15 +1,15 @@
 import NavBar from "../../components/navbar/Navbar";
 import Hero from "../../components/hero/Hero";
 import styles from "./HomePage.module.css";
+import Footer from "./../../components/footer/Footer";
 
 const HomePage = () => {
 	return (
 		<>
 			<NavBar />
-			<div className= {styles.home}>
-				<Hero />
-			</div>
-			
+			<div className={styles.home}></div>
+			<Hero />
+			<Footer />
 		</>
 	);
 };
